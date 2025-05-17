adb931cf84eda32c3b94416be0634e971c2336e2
L-Spellmeyer <156337140+L-Spellmeyer@users.noreply.github.com>
2025-05-17 12:37:25 -0500

    Create a program to simulate the tortoise and hare story using conditionals and loops

---
+++ Intro Book/Notable .py/race.py
@@ -0,0 +1,78 @@
+import random
+import time
+
+def tortoise(intCurrentSquare):
+    intRandomMove = random.randrange(1,11)
+
+    if (1 <= intRandomMove and intRandomMove <= 5):
+        return intCurrentSquare + 3
+    elif (6 <= intRandomMove and intRandomMove <= 7):
+        return intCurrentSquare - 6
+    elif (8 <= intRandomMove and intRandomMove <= 10):
+        return intCurrentSquare + 1
+
+
+def hare(intCurrentSquare):
+    intRandomMove = random.randrange(1,11)
+
+    if (1 == intRandomMove or intRandomMove == 2):
+        return intCurrentSquare
+    elif (3 == intRandomMove or intRandomMove == 4):
+        return intCurrentSquare + 9
+    elif (intRandomMove == 5):
+        return intCurrentSquare - 12
+    elif (intRandomMove >= 6 and intRandomMove <= 8):
+        return intCurrentSquare + 1
+    elif(intRandomMove >= 9):
+        return intCurrentSquare - 2
+
+
+
+
+def main():
+    input("The race will start on your call! ")
+    print("BANG ! ! ! ! !")
+    print("And they're off!!")
+
+
+    intTortoiseLocation = 1
+    intHareLocation = 1
+    boolWinner  = False
+
+
+    while(boolWinner == False):
+        intTortoiseLocation = tortoise(intTortoiseLocation)
+        intHareLocation = hare(intHareLocation)
+
+
+        if (intHareLocation < 1):
+            intHareLocation = 1
+        if(intTortoiseLocation < 1):
+            intTortoiseLocation = 1
+        
+        # Debug statement to check location ints
+        print(intTortoiseLocation, " ", intHareLocation)
+
+        print(" ")
+        for char in range(70):
+            if (intTortoiseLocation == char):
+                print("T", end=" ")
+            elif (intHareLocation == char):
+                print("H", end=" ")
+            else:
+                print("-", end=" ")
+
+        if (intTortoiseLocation >= 70):
+            print("\nTORTOISE WINS!!! YAY!!!")
+            boolWinner = True
+        elif (intHareLocation >= 70):
+            print("\nHare wins. Yuck")
+            boolWinner = True
+        
+        time.sleep(.5)
+
+
+
+
+    return 0
+main()
