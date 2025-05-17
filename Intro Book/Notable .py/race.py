61fdf6f0a15ccb033c758cea79b6d5b8c81fb167
L-Spellmeyer <156337140+L-Spellmeyer@users.noreply.github.com>
2025-05-17 12:40:42 -0500

    Added comments to code

--- Intro Book/Notable .py/race.py
+++ Intro Book/Notable .py/race.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+# This function calculates and updates the tortois's location in the race
 def tortoise(intCurrentSquare):
     intRandomMove = random.randrange(1,11)
 
@@ -11,7 +12,7 @@ def tortoise(intCurrentSquare):
     elif (8 <= intRandomMove and intRandomMove <= 10):
         return intCurrentSquare + 1
 
-
+# Same funcionality as tortoise function but for the hare
 def hare(intCurrentSquare):
     intRandomMove = random.randrange(1,11)
 
@@ -39,7 +40,7 @@ def main():
     intHareLocation = 1
     boolWinner  = False
 
-
+# While loop that runs the race until the T or H crosses the finish line (has a location >= 70)
     while(boolWinner == False):
         intTortoiseLocation = tortoise(intTortoiseLocation)
         intHareLocation = hare(intHareLocation)
@@ -69,10 +70,7 @@ def main():
             print("\nHare wins. Yuck")
             boolWinner = True
         
+        #Added a sleep timer so the programs progression could be followed more easily
         time.sleep(.5)
-
-
-
-
     return 0
 main()
